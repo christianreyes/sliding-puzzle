9ede81120b48a4aaaf940795320b42f90fe42e91
Christian Reyes <christian.reyes.91@gmail.com>
2018-01-15 21:54:19 -0600

    faster shuffle

--- docs/p1.js
+++ docs/p1.js
@@ -240,11 +240,11 @@ function shuffleTiles(numLeft) {
         }
 
         var t = getTile(selectedTile.row, selectedTile.col);
-        t.slide(50);
+        t.slide(5);
         _just_moved = t;
         
         // keep shuffling
-        setTimeout(function () { shuffleTiles(numLeft - 1) }, 250);
+        setTimeout(function () { shuffleTiles(numLeft - 1) }, 50);
     } else {
         // when the shuffling is done, set puzzle so tiles can be clicked
         _able_to_click = true;
